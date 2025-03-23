ffc261a043b2bd1f2b638b141ce4a269cb2fbc41
Nino-FR-04 <davidfrx322@gmail.com>
2025-03-23 10:43:07 -0500

    Actualizacion Clase Golosina

--- Actividades/Golosina.java
+++ Actividades/Golosina.java
@@ -1,4 +1,18 @@
 public class Golosina {
+    //Atributos
+    private String nombre;
+    private double peso;
 
-    
+    //Constructor
+    public Golosina(String nombre, double peso){
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    //Setters y getters
+    public String getNombre(){return this.nombre;}
+    public void setNombre(String nombre){this.nombre = nombre;}
+
+    public double getPeso(){return this.peso;}
+    public void setPeso(double peso){this.peso = peso;}
 }
