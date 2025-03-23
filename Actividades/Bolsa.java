25b27d1ee24b41fc9be017c208fb7891e428ad5b
Nino-FR-04 <davidfrx322@gmail.com>
2025-03-22 21:50:16 -0500

    Actualizacion Clase Bolsa

--- Actividades/Bolsa.java
+++ Actividades/Bolsa.java
@@ -1,3 +1,28 @@
-public class Bolsa{
-    
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bolsa <E> implements Iterable <E> {
+    //Atributos
+    private ArrayList <E> lista = new ArrayList <E>();
+    private int tope; //Reresenta la cantidad maxima de elementos
+
+    //Constructor
+    public Bolsa(int tope){
+        //super(); -> Codigo innecesario
+        this.tope = tope;
+    }
+
+    //Metodo Encargado de añadir a la bolsa
+    public void add(E objeto) {
+        if(this.lista.size() <= this.tope) {
+            this.lista.add(objeto);
+        }else{
+            throw new RuntimeException("Limite Alcanzado");
+        }
+    }
+
+    //Metodo que permite recorrer "Bolsa"
+    public Iterator <E> iterator() {
+        return this.lista.iterator();
+    }
 }
