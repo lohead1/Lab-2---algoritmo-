85dc37297868d45bb9c6cef059701ccb43fb3147
Nino-FR-04 <davidfrx322@gmail.com>
2025-03-23 10:46:27 -0500

    Actualizacion Clase Chocolatina

--- Actividades/Chocolatina.java
+++ Actividades/Chocolatina.java
@@ -1,3 +1,13 @@
 public class Chocolatina {
+    //Atributo
+    private String marca;
+    
+    //Constructor
+    public Chocolatina(String marca){
+        this.marca = marca;
+    }
 
+    //Setters y Getters
+    public String getMarca(){return this.marca;}
+    public void setMarca(String marca){this.marca = marca;}
 }
